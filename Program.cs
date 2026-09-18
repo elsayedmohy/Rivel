8f1e53b41a8f7b5c090d72e7d4b9736f037f4749
s.mohy <bdwym217@gmail.com>
2026-09-18 03:09:27 +0300

    Add Cors

--- Program.cs
+++ Program.cs
@@ -8,7 +8,8 @@ builder.AddControllers()
     .AddErrorHandling()
     .AddDatabase()
     .AddApplicationServices()
-    .AddAuthenticationServices();
+    .AddAuthenticationServices()
+    .AddCors();
 
 var app = builder.Build();
 
@@ -25,7 +26,7 @@ app.UseExceptionHandler();
 app.UseStatusCodePages();
 
 app.UseHttpsRedirection();
-
+app.UseCors("RiverLinePolicy"); 
 app.UseAuthentication();
 app.UseAuthorization();
 
